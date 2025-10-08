cef031dd45a011f166ca0c8b8f827d7d1ddfad6f
Mika Klepper <mika.klepper@ru.nl>
2025-10-08 15:29:03 +0200

    changed jobs to output good log location

--- jobs/slide_2_vec_liver_ex.sh
+++ jobs/slide_2_vec_liver_ex.sh
@@ -5,11 +5,13 @@
 #SBATCH --mem=30G
 #SBATCH --time=7-00:00:00
 #SBATCH --job-name="l-uni"
-#SBATCH --output=/data/temporary/mika/logs/slurm-%j-l-uni.out
+#SBATCH --output=/data/temporary/mika/repos/oaks_project/logs/slurm-%j-l-uni.out
 #SBATCH --container-mounts=/data/pa_cpgarchive:/data/pa_cpgarchive,/data/temporary:/data/temporary
 #SBATCH --container-image="dockerdex.umcn.nl:5005#clemsgrs/slide2vec:v1.2.3"
 #SBATCH --qos=high
 #SBATCH --requeue
+
+
 
 # === Environment setup ===
 export HF_TOKEN="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"
